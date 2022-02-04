18ab085a5fd7e54a5393bed559c671b8c6448f04
fabian lim <flqf1217@hotmail.com>
2022-02-04 12:07:59 +0800

    main branch

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main(){
 	
-	persons := getPersons()
-
-	for i, _ := range persons {
-		persons[i].Name = persons[i].Name + "\n this is name"
-	}
-
-	fmt.Println(persons)
 }
